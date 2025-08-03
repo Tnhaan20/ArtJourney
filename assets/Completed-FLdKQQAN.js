defb41e5ad27ab7e995afff5b90a37819081760b
Tnhaan20 <118607754+Tnhaan20@users.noreply.github.com>
2025-08-03 12:24:22 +0000

    Deploying to public from @ Tnhaan20/ArtJourney@42e141f81899b879894036a77c2f3388c54a255e 🚀

--- assets/Completed-FLdKQQAN.js
+++
@@ -1,6 +0,0 @@
-import{d as o,j as t}from"./index-75yQjoJZ.js";/**
- * @license lucide-react v0.511.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const s=[["path",{d:"M12 20h9",key:"t2du7b"}],["path",{d:"M16.376 3.622a1 1 0 0 1 3.002 3.002L7.368 18.635a2 2 0 0 1-.855.506l-2.872.838a.5.5 0 0 1-.62-.62l.838-2.872a2 2 0 0 1 .506-.854z",key:"1ykcvy"}]],d=o("pen-line",s);function a({isCompleted:r,className:n="",size:e=20}){return t.jsx("div",{className:`rounded-full flex items-center justify-center ${r?"bg-green-500":"border border-gray-300 bg-white"} ${n}`,style:{width:`${e}px`,height:`${e}px`},children:r&&t.jsx("svg",{xmlns:"http://www.w3.org/2000/svg",className:"text-white",style:{width:`${e*.6}px`,height:`${e*.6}px`},fill:"none",viewBox:"0 0 24 24",stroke:"currentColor",children:t.jsx("path",{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:3,d:"M5 13l4 4L19 7"})})})}export{a as C,d as P};
